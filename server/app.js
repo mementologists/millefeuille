3f6d16fa3138648bfebc1c4951c4f681264cb5ed
azmeer <azmeer.salleh@gmail.com>
2017-07-17 11:46:28 -0700

    Strips repo of auth and passport, resolves pomander warnings
    removes client and webpack config and unneeded packages.
    removes auth and passport
    updates package versions to resolve pomander warnings

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-'use strict';
 const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
@@ -8,19 +7,15 @@ const app = express();
 
 app.use(middleware.morgan('dev'));
 app.use(middleware.cookieParser());
-app.use(middleware.bodyParser.urlencoded({extended: false}));
+app.use(middleware.bodyParser.urlencoded({ extended: false }));
 app.use(middleware.bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(middleware.auth.session);
-app.use(middleware.passport.initialize());
-app.use(middleware.passport.session());
 app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', routes.auth);
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-'use strict';
 const app = require('./app');
-const db = require('../db');
+
+// const db = require('../db');
+
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
+  /* eslint-disable no-console */
+  console.log('millefeuille server listening on port 3000!');
+  /* eslint-enable no-console */
 });
