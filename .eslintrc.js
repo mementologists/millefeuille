3e2ea7bd4de7be4860456f1e49d4e2b7b52cc332
azmeer <azmeer.salleh@gmail.com>
2017-07-17 14:00:46 -0700

    removes hackreactor eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,8 @@
-/**
- * These rules enforce Hack Reactor's style guide.
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
   env: {
     'es6': true
   },
   extends: [
-    './node_modules/eslint-config-hackreactor/index.js',
     './node_modules/eslint-config-airbnb/index.js' 
   ],
   rules: {
