f183632cb28b7a3bd146f6c24e428d29b8f36451
azmeer <azmeer@users.noreply.github.com>
2017-07-24 18:15:18 -0700

    adds sentiment summarization for text/audio/watson-sentiment analysis (#8)
    This feature includes the database functions and completes the analysis
    server plugin to summarize Watson-based sentiment analyses.

--- db/index.js
+++ db/index.js
@@ -1,4 +1,17 @@
+const config = require('config');
+
+const Promise = require('bluebird');
+
+const mongoose = require('mongoose');
+
+const mongoUri = process.env.MONGODB_URI || config.servers.mongo.uri;
+
+const options = { promiseLibrary: Promise };
+
+mongoose.connect(mongoUri, options);
+
+mongoose.Promise = Promise;
+
 const models = require('./models');
 
 module.exports = models;
-
--- server/plugins/text.js
+++ server/plugins/text.js
@@ -1,12 +1,14 @@
 const textAnalysis = require('../lib/toneAsync').text;
 
+const utils = require('../lib/processUtils');
+
 const scrubText = text =>
   text.replace(/[^]+name="file"/, '').replace(/------WebKitFormBoundary[^]*/, '');
 
 module.exports.processData = moment =>
   textAnalysis(scrubText(moment.media.text))
   .then((data) => {
-    const newMoment = JSON.parse(JSON.stringify(moment));
+    const newMoment = utils.clone((moment));
     newMoment.media.text = data;
     return newMoment;
   });
