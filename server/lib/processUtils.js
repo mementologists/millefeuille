051f2d73d9b177c67c10cf7376c882b66ada91da
azmeer <azmeer.salleh@gmail.com>
2017-07-25 11:37:23 -0700

    Fixes dependency on db for non-analysis process servers and fixes clone()
    fixes clone to use Object.assign instead of JSON.parse(JSON.stringify())
    removes dependency on db for non-analysis process servers

--- server/lib/processUtils.js
+++ server/lib/processUtils.js
@@ -2,7 +2,7 @@
 const getMaxIndex = array =>
       array.findIndex(el => el.score === Math.max(...array.map(tone => tone.score)));
 
-module.exports.clone = obj => JSON.parse(JSON.stringify(obj));
+module.exports.clone = obj => Object.assign({}, obj);
 
 module.exports.textSummary = (textAnalysis) => {
   const tones = textAnalysis.document_tone.tone_categories[0].tones;
