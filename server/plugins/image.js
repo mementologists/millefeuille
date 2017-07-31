9a569c1d8d089424b9a95b1dcffc4732fe4161e2
azmeer <azmeer@users.noreply.github.com>
2017-07-30 19:04:06 -0700

    adds Vision analysis of images (#20)

--- server/plugins/image.js
+++ server/plugins/image.js
@@ -6,22 +6,34 @@ const utils = require('../lib/processUtils');
 
 const config = require('config').servers.services.image;
 
-const axiosCfg = {
-  headers: {
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': config.key
-  }
+const vizParams = {
+  visualFeatures: 'Categories,Tags,Faces,ImageType,Description,Color,Adult',
+  details: 'Celebrities,Landmarks',
+  language: 'en'
 };
 
 module.exports.fetchData = req => Promise.resolve(req.body.moment);
 
 module.exports.processData = moment =>
-  axios.post(config.api_uri, { url: moment.media.image.uri }, axiosCfg)
-  .then(res => utils.summarizeImage(res.data))
+  Promise.all(
+    [
+      axios.post(config.api_uri,
+        { url: moment.media.image.uri }, utils.getCfg(config.key)),
+      axios.post(utils.getParamURI(config.vision.api_uri, vizParams),
+        { url: moment.media.image.uri }, utils.getCfg(config.vision.key))
+    ])
+  .then(([emoRes, vizRes]) => {
+    const aggregate = utils.summarizeImage(emoRes.data);
+    aggregate.description = vizRes.data;
+    return aggregate;
+  })
   .then((aggregate) => {
     const newMoment = utils.clone(moment);
     newMoment.media = {};
     newMoment.media.image = aggregate;
     return newMoment;
+  })
+  .catch((err) => {
+    console.log('Image ProcessData Error: ', err); // eslint-disable-line no-console
   });
 
